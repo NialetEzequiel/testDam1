b662632a566e725d88c811318c3a2e47cb56f596
NialetEzequiel <ezekiel.nialet@gmail.com>
2023-05-26 20:27:36 +0200

    bug found

--- src/testGit.java
+++ src/testGit.java
@@ -2,5 +2,7 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola Mundo!");
+        //Bug
+        System.out.println("Bug found");
     }
 }
