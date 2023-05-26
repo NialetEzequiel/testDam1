da9af940f0041faba5e36808244db9e04691d8d0
NialetEzequiel <ezekiel.nialet@gmail.com>
2023-05-26 20:29:56 +0200

    comment added.

--- src/testGit.java
+++ src/testGit.java
@@ -2,5 +2,6 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola Mundo!");
+        //Bugfix here
     }
 }
